e754dde19cae3ec794c34d725bb3dc8e6cdad1d8
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-05-13 15:36:07 +0900

    add stdout

--- lib/cg/server.rb
+++ lib/cg/server.rb
@@ -11,11 +11,14 @@ module CG
 
     def call(env)
       env['PATH_INFO'] = '/index.html' if  env['PATH_INFO'] == '/'
+      puts env['PATH_INFO']
+
       @file_server.call(env)
     end
   end
 end
 
 if __FILE__ == $0
+  puts "[#{Time.now.strftime('%Y-%m-%d %H:%m')}] cg server start. http://localhost:9292"
   Rack::Handler::default.run CG::Server.new, :Port => 9292
 end
